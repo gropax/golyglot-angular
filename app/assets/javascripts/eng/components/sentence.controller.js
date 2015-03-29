a6d10273e1cdf3a589fc4e46c3eeab1bc82acfe3
Maxime Laudrin <maximedelaudrin@gmail.com>
2015-03-29 15:49:17 +0200

    Fixed English sentence display

--- app/assets/javascripts/eng/components/sentence.controller.js
+++ app/assets/javascripts/eng/components/sentence.controller.js
@@ -1,6 +1,8 @@
 angular.module('golyglot.eng').controller('EngSentenceCtrl', EngSentenceCtrl);
 
 function EngSentenceCtrl($scope, eng) {
-    $scope.english = eng.getEnglish($scope.sentence);
+    var reprs = $scope.sentence.textRepresentations;
+
+    $scope.english = eng.getEnglish(reprs);
 };
 

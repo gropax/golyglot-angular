8120ddf26e6a7bff0dc3f45a618f2ebc10298e35
gropax <maximedelaudrin@gmail.com>
2015-04-30 14:37:36 +0200

    Added passing specs for ggEditLemmaForm directive's #valid and HTML

--- app/assets/javascripts/lexical-entries/components/edit-lemma-form/directive.js
+++ app/assets/javascripts/lexical-entries/components/edit-lemma-form/directive.js
@@ -17,6 +17,8 @@ function ggEditLemmaForm() {
             $scope.representations = $scope.original.clone().representations;
 
             $scope.submit = function() {
+                if (!$scope.valid) { return false; }
+
                 updateLemma().then(function(result) {
                     // Replace the old one by the new
                     $scope.original = new Lemma(result);
@@ -27,9 +29,51 @@ function ggEditLemmaForm() {
                 });
             };
 
+
+            $scope.$watch('representations', function(reprs) {
+                var blank = true,
+                    ident = true;
+
+                forEachRepresentation(function(newRepr, oldRepr) {
+                    ident = ident && (newRepr.writtenForm === oldRepr.writtenForm);
+                    blank = blank && (newRepr.writtenForm === "");
+                }, function(newRepr) {
+                    blank = blank && (newRepr === "");
+                });
+
+                $scope.valid = !blank && !ident;
+            });
+
+
             function updateLemma() {
                 var reqReprs = [];
 
+                forEachRepresentation(function(newRepr, oldRepr) {
+                // New repr match a old one
+                    if (newRepr.writtenForm === "") {
+                        // Delete
+                        reqReprs.push({id: oldRepr.id, _destroy: "1"});
+                    } else if (newRepr.writtenForm !== oldRepr.writtenForm) {
+                        // Update
+                        reqReprs.push({id: oldRepr.id, writtenForm: newRepr.writtenForm});
+                    }
+                // New repr do not match a old one
+                }, function(newRepr) {
+                    // Create
+                    reqReprs.push(newRepr);
+                });
+
+                var lemma =  new Lemma({
+                    id: $scope.original.id,
+                    lexicalEntryId: $scope.original.lexicalEntryId,
+                    representations: reqReprs,
+                })
+
+                return lemma.update();
+            }
+
+
+            function forEachRepresentation(match, noMatch) {
                 for (var i = 0 ; i < $scope.representations.length ; i++) {
                     var newRepr = $scope.representations[i];
 
@@ -45,27 +89,13 @@ function ggEditLemmaForm() {
                     }
 
                     if (correspondingRepr) {
-                        if (newRepr.writtenForm === "") {
-                            // Delete
-                            reqReprs.push({id: correspondingRepr.id, _destroy: "1"});
-                        } else if (newRepr.writtenForm !== correspondingRepr.writtenForm) {
-                            // Update
-                            reqReprs.push({id: correspondingRepr.id, writtenForm: newRepr.writtenForm});
-                        }
+                        match.call(this, newRepr, correspondingRepr);
                     } else {
-                        // Create
-                        reqReprs.push(newRepr);
+                        noMatch.call(this, newRepr);
                     }
                 }
-
-                var lemma =  new Lemma({
-                    id: $scope.original.id,
-                    lexicalEntryId: $scope.original.lexicalEntryId,
-                    representations: reqReprs,
-                })
-
-                return lemma.update();
             }
-        },
+
+        }
     };
 }
--- spec/javascripts/lexical-entries/components/edit-lemma-form.directive.spec.js
+++ spec/javascripts/lexical-entries/components/edit-lemma-form.directive.spec.js
@@ -13,7 +13,7 @@
 
                 $scope = $rootScope.$new();
                 // Create HTML code using the directive
-                element = '<div gg-edit-lemma-form lang-code="langCode" model="entry.lemma" on-success="callback"></div>';
+                element = '<div gg-edit-lemma-form lang-code="langCode" model="entry.lemma" on-success="callback()"></div>';
                 // Populate the $scope with data
                 $scope.langCode = 'cmn';
 
@@ -53,6 +53,7 @@
                 element = $compile(element)($scope);
                 // Must trigger a `$digest` before testing
                 $scope.$digest();
+
                 isolated = element.isolateScope();
             });
         });
@@ -63,8 +64,19 @@
         });
 
 
+        var valid = {
+            representations: [{id: "000", script: "Hans", orthographyName: "simplified", writtenForm: "XX"}, {id: "111", script: "Hant", orthographyName: "traditional", writtenForm: ""}, {id: "222", script: "Latn", orthographyName: "pinyin", writtenForm: "ni3hao3ma"}, {script: "IPA", orthographyName: "IPA", writtenForm: "ni214Xaw214ma"}],
+            request: {lemma: {id: "789", lexical_entry_id: "123", representations: [{id: "111", _destroy: "1"}, {id: "222", written_form: "ni3hao3ma"}, {script: "IPA", orthography_name: "IPA", written_form: "ni214Xaw214ma"}]}},
+            response: {lemma: {id: "789", lexical_entry_id: "123", representations: [{id: "000", script: "Hans", orthographyName: "simplified", writtenForm: "XX"}, {id: "222", script: "Latn", orthographyName: "pinyin", writtenForm: "ni3hao3ma"}, {id: "333", script: "IPA", orthography_name: "IPA", written_form: "ni214Xaw214ma"}]}},
+        };
+
+        var identical = {
+            representations: [{id: "000", script: "Hans", orthographyName: "simplified", writtenForm: "XX"}, {id: "111", script: "Hant", orthographyName: "traditional", writtenForm: "YY"}, {id: "222", script: "Latn", orthographyName: "pinyin", writtenForm: "ni3hao3"}],
+        };
+
 
         describe("isolated scope", function() {
+
             describe("#representations", function() {
                 it("should be a copy of the model lemma representations", function() {
                     var original = $scope.entry.lemma.representations;
@@ -76,125 +88,111 @@
                 });    
             });
 
-            describe("when valid data", function() {
 
-                beforeEach(function() {
-                    // Modify the representations copy as would the language form do.
-                    isolated.representations = [
-                        { // Untouched
-                            id: "000",
-                            script: "Hans",
-                            orthographyName: "simplified",
-                            writtenForm: "XX",
-                        },
-                        { // Emptied
-                            id: "111",
-                            script: "Hant",
-                            orthographyName: "traditional",
-                            writtenForm: "",
-                        },
-                        { // Modified
-                            id: "222",
-                            script: "Latn",
-                            orthographyName: "pinyin",
-                            writtenForm: "ni3hao3ma",
-                        },
-                        { // Added
-                            script: "IPA",
-                            orthographyName: "IPA",
-                            writtenForm: "ni214Xaw214ma",
-                        },
-                    ];
-                });
+            describe("#submit", function() {
 
-                describe("#submit", function() {
+                describe("when valid data", function() {
+                    beforeEach(function() {
+                        isolated.representations = valid.representations;
+                        $scope.$digest();
+                        $httpBackend.whenPUT('/api/lexical_entries/123/lemma', valid.request ).respond(200, valid.response);
+                    });
+
                     it("should PUT /api/lexical_entries/:id/lemma", function() {
-                        $httpBackend.expectPUT('/api/lexical_entries/123/lemma', {
-                            lemma: {
-                                id: "789",
-                                lexical_entry_id: "123",
-                                representations: [
-                                    {
-                                        id: "111",
-                                        _destroy: "1",
-                                    },
-                                    {
-                                        id: "222",
-                                        written_form: "ni3hao3ma",
-                                    },
-                                    {
-                                        script: "IPA",
-                                        orthography_name: "IPA",
-                                        written_form: "ni214Xaw214ma",
-                                    },
-                                ]
-                            }
-                        }).respond(200);
-
-                        $scope.$apply(function() {
-                            isolated.submit();
-                        });
-
+                        $httpBackend.expectPUT('/api/lexical_entries/123/lemma');
+                        $scope.$apply(function() { isolated.submit(); });
                         $httpBackend.flush();
                     });
 
                     it("should update the original", function() {
-                        var result = {
-                            lemma: {
-                                id: "789",
-                                lexical_entry_id: "123",
-                                representations: [
-                                    {
-                                        id: "000",
-                                        script: "Hans",
-                                        orthographyName: "simplified",
-                                        writtenForm: "XX",
-                                    },
-                                    {
-                                        id: "222",
-                                        script: "Latn",
-                                        orthographyName: "pinyin",
-                                        writtenForm: "ni3hao3ma",
-                                    },
-                                    {
-                                        id: "333",
-                                        script: "IPA",
-                                        orthography_name: "IPA",
-                                        written_form: "ni214Xaw214ma",
-                                    },
-                                ]
-                            }
-                        };
-
-                        $httpBackend.expectPUT('/api/lexical_entries/123/lemma').respond(200, result);
-
-                        $scope.$apply(function() {
-                            isolated.submit();
-                        });
-
+                        $scope.$apply(function() { isolated.submit(); });
                         $httpBackend.flush();
 
-                        var updatedLemma = new Lemma(result.lemma);
+                        var updatedLemma = new Lemma(valid.response.lemma);
                         expect($scope.entry.lemma).toEqual(updatedLemma);
                     });
 
                     it("should call the callback", function() {
+                        $scope.$apply(function() { isolated.submit(); });
+                        $httpBackend.flush();
                         expect($scope.callback).toHaveBeenCalled();
+                    });
+                });
+
+                describe("when invalid data", function() {
+                    beforeEach(function() {
+                        isolated.representations = identical.representations;
+                    });
+
+                    it("should not call the callback", function() {
+                        $scope.$apply(function() { isolated.submit(); });
+                        expect($scope.callback).not.toHaveBeenCalled();
                     });
                 });
 
             });
 
+
+            describe("#valid", function() {
+
+                describe("when valid data", function() {
+                    beforeEach(function() {
+                        isolated.representations = valid.representations;
+                        $scope.$digest();
+                    });
+
+                    it("should return `true`", function() {
+                        expect(isolated.valid).toBe(true);
+                    });
+                });
+
+                describe("when identical to original", function() {
+                    beforeEach(function() {
+                        isolated.representations = identical.representations;
+                        $scope.$digest();
+                    });
+
+                    it("should return `false`", function() {
+                        expect(isolated.valid).toBe(false);
+                    });
+                });
+
+                var blank = {
+                    representations: [{id: "000", script: "Hans", orthographyName: "simplified", writtenForm: ""}, {id: "111", script: "Hant", orthographyName: "traditional", writtenForm: ""}, {id: "222", script: "Latn", orthographyName: "pinyin", writtenForm: ""}],
+                };
+
+                describe("when all blank", function() {
+                    beforeEach(function() {
+                        isolated.representations = blank.representations;
+                        $scope.$digest();
+                    });
+
+                    it("should return `false`", function() {
+                        expect(isolated.valid).toBe(false);
+                    });
+                });
+
+            });
+
+
         });
 
         describe("HTML", function() {
-            describe("button's class", function() {
+
+            describe("button", function() {
                 it("should be disabled if form invalid", function() {
+                    isolated.valid = false;
+                    $scope.$digest();
+                    expect(element.find('button').hasClass('disabled')).toBe(true);
                 });
 
                 it("should be enabled if form valid", function() {
+                    isolated.valid = true;
+                    $scope.$digest();
+                    expect(element.find('button').hasClass('disabled')).toBe(false);
                 });
             });
+
         });
         
     });
